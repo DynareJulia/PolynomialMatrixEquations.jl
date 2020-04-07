a28d9fe78b68d937f7ed4b53f1ed31fdc55c1c2b
Michel Juillard <michel@debian.home>
2020-04-07 12:09:26 +0200

    fix exceptions

--- src/PolynomialMatrixEquations.jl
+++ src/PolynomialMatrixEquations.jl
@@ -1,5 +1,9 @@
 module PolynomialMatrixEquations
 
+struct UnstableSystemException <: Exception end
+struct UndeterminateSystemException <: Exception end
+export UnstableSystemException, UndeterminateSystemException
+
 include("CyclicReduction.jl")
 export CyclicReductionWs, cyclic_reduction!, cyclic_reduction_check
 
